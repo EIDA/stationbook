5e351f1d0446788116ef344c2bd4503dcfa00d84
Jarosław Bieńkowski <jaroslaw.bienkowski@hotmail.com>
2018-02-01 12:57:08 +0000

    Show stations on the map

--- src/stationbook/static/js/home_openlayers.js
+++ src/stationbook/static/js/home_openlayers.js
@@ -1,32 +1,67 @@
+var vectorSource = new ol.source.Vector({
+  features: []
+});
+
+var vectorLayer = new ol.layer.Vector({
+  source: vectorSource
+});
+
 var map = new ol.Map({
-    target: 'map',
-      layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
     }
-    )
-    ],
-    controls: ol.control.defaults({
-      attributionOptions: {
-        collapsible: false
-      }
-    }
-    ),
-    target: 'map',
-      view: new ol.View({
-        center: ol.proj.fromLonLat([5.178029, 52.101568]),
-          zoom: 16
-    }
-    )
+    ), vectorLayer
+  ],
+  target: document.getElementById('map'),
+  view: new ol.View({
+    center: ol.proj.fromLonLat([5.178029, 52.101568]),
+    zoom: 16
   }
-  );
-  var source = new ol.source.Vector({
-    wrapX: false
+  )
+}
+);
+
+var element = document.getElementById('popup');
+
+var popup = new ol.Overlay({
+  element: element,
+  positioning: 'bottom-center',
+  stopEvent: false,
+  offset: [0, -50]
+});
+map.addOverlay(popup);
+
+// display popup on click
+map.on('click', function (evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+    function (feature) {
+      return feature;
+    });
+  if (feature) {
+    // Make sure the popup is disposed (when clicking new marker before
+    // deselecting the previous one, popup was showing previous marker data.
+    $(element).popover('dispose');
+    var coordinates = feature.getGeometry().getCoordinates();
+    popup.setPosition(coordinates);
+    $(element).popover({
+      'placement': 'top',
+      'html': true,
+      'content': feature.get('name')
+    });
+    $(element).popover('show');
+  } else {
+    $(element).popover('dispose');
   }
-  );
-  var vector = new ol.layer.Vector({
-    source: source
+});
+
+// change mouse cursor when over marker
+map.on('pointermove', function (e) {
+  if (e.dragging) {
+    $(element).popover('dispose');
+    return;
   }
-  );
-  map.addLayer(vector);
-  
+  var pixel = map.getEventPixel(e.originalEvent);
+  var hit = map.hasFeatureAtPixel(pixel);
+  map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
