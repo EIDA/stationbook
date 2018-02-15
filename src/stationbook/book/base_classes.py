56c410f7551bcd37e087302b951c7b2298f8fafb
Jarosław Bieńkowski <jaroslaw.bienkowski@hotmail.com>
2018-02-15 15:18:48 +0000

    Added small methods to check if user can edit network or station

--- src/stationbook/book/base_classes.py
+++ src/stationbook/book/base_classes.py
@@ -18,6 +18,26 @@ class StationBookHelpers():
             StationBookLogger(__name__).log_exception(
                 add_ext_access_data.__name__)
 
+    @staticmethod
+    def user_is_network_editor(user, network):
+        try:
+            if user.profile in network.eitors.all():
+                return True
+            else:
+                return False
+        except:
+            return False
+    
+    @staticmethod
+    def user_is_station_editor(user, station):
+        try:
+            if user.profile in station.fdsn_network.eitors.all():
+                return True
+            else:
+                return False
+        except:
+            return False
+
 class StationUpdateViewBase(UpdateView):
     def __init__(self, model, fields,
         template_name='station_edit.html',
