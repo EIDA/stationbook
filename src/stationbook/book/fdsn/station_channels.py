0b9e00f8ee4129788ca5c77c6b749a0132edcb06
Jarosław Bieńkowski <jaroslaw.bienkowski@hotmail.com>
2018-02-22 14:49:05 +0000

    Added initial migration file and modified code in station_channels for test cases

--- src/stationbook/book/fdsn/station_channels.py
+++ src/stationbook/book/fdsn/station_channels.py
@@ -17,6 +17,14 @@ class StationChannelsGraph(object):
     def get_station_channels(self):
         channels_graph = StationChannels()
         response = urlopen(self.url)
+
+        http_code = response.getcode()
+
+        # If something went wrong, log it and return empty dataset
+        if http_code != 200:
+            StationBookLogger(__name__).log_exception('{0} from {1} in {2}'.format(http_code, self.url, StationChannelsGraph.__name__))
+            return StationChannels()
+
         root = ET.fromstring(response.read())
 
         try:
