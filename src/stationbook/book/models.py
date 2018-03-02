e58ede76f3392f0a31aba9a36c35d5200ee45f7e
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2018-03-02 16:57:50 +0100

    Use network.get_code_year to show user assigned networks

--- src/stationbook/book/models.py
+++ src/stationbook/book/models.py
@@ -320,6 +320,7 @@ class Profile(models.Model):
     def __str__(self):
         return 'Profile of: {0}'.format(self.user)
 
+
 class Link(models.Model):
     url = models.CharField(max_length=STRING_LENGTH_MEDIUM, null=True, blank=True)
     category = models.CharField(max_length=STRING_LENGTH_MEDIUM, null=True, blank=True)
