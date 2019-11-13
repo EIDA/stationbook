42d19448c22fbdbfabfcf05b80ab4aa4a3013478
Jarek Bienkowski <jarek@MacJarek.local>
2019-11-13 13:24:35 +0100

    Added old data exports

--- src/scripts/old_station_book_export.sql
+++ src/scripts/old_station_book_export.sql
@@ -1,12 +1,19 @@
 -- Get data from SB1
+ SELECT
+	*
+FROM
+	StationDescription SD
+INNER JOIN SensorLocationDescription SLD ON
+	SD.`_oid` = SLD.`_parent_oid`
+LEFT JOIN NetworkDescription ND ON
+	SD.`_parent_oid` = ND.`_oid`
+LEFT JOIN BoreHole BH ON
+	SD.`_oid` = BH.`_parent_oid`
+LEFT JOIN StationOwner SO ON
+	SD.`_oid` = SO.`_parent_oid`
+
+-- Get photo information
 SELECT
-*
-FROM StationDescription SD
-	INNER JOIN SensorLocationDescription SLD
-		ON SD.`_oid` = SLD.`_parent_oid`
-	LEFT JOIN NetworkDescription ND
-		ON SD.`_parent_oid` = ND.`_oid`
-	LEFT JOIN BoreHole BH
-		ON SD.`_oid` = BH.`_parent_oid`
-	LEFT JOIN StationOwner SO
-		ON SD.`_oid` = SO.`_parent_oid`
+	*
+FROM
+	StationCharacterizationFileResource
