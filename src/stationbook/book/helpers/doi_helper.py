43cbb857226d7db2a1ff82e5c7e746b2515a269e
Joao Zanetti <20665036+jppzanetti@users.noreply.github.com>
2021-01-28 09:25:24 +0100

    Getting network DOI from FDSN WS, getting rid of citation scraping.

--- src/stationbook/book/helpers/doi_helper.py
+++ src/stationbook/book/helpers/doi_helper.py
@@ -1,8 +1,9 @@
 import gzip
+import json
 from urllib.request import Request, urlopen
 from django.core.cache import cache
 from ..logger import StationBookLoggerMixin
-from bs4 import BeautifulSoup
+from datetime import date
 
 
 class DOIHelper(StationBookLoggerMixin):
@@ -14,74 +15,32 @@ class DOIHelper(StationBookLoggerMixin):
             dois = None
 
             if not cache.get("network_dois"):
-                response = self._request("http://fdsn.org/networks/doi")
-                decoded = response.decode("utf-8")
-                dois = decoded.split("\r\n")
+                response = self._request("http://fdsn.org/ws/networks/1/query")
+                dois = {}
+                for net in response["networks"]:
+                    network_key = net["fdsn_code"][:]
+                    if net["end_date"]:
+                        start_year = date.fromisoformat(net["start_date"]).strftime("%Y")
+                        network_key = network_key + "_{}".format(start_year)
+
+                    dois[network_key] = net["doi"]
                 cache.set("network_dois", dois, 86400)
             else:
                 dois = cache.get("network_dois")
 
-            # Filter using network code and network start year
-            doi = list(
-                filter(
-                    lambda x: x.startswith(
-                        "{}_{}".format(network_code, network_start_year)
-                    ),
-                    dois,
-                )
-            )
+            # Filter using network code and maybe network start year
+            doi = dois.get(network_code)
+            if doi is None:
+                doi = dois.get("{}_{}".format(network_code, network_start_year))
 
-            # In case previous filter did not return any results, use less
-            # restrictive filtering using just the network code
-            # NL network falls in this category, for example
-            if len(doi) <= 0:
-                doi = list(
-                    filter(lambda x: x.startswith("{}".format(network_code)), dois)
-                )
+            if not doi:
+                return None
 
-            doi = doi[0].split(",")[1]
             doi = "https://www.doi.org/{}".format(doi)
             return doi
         except Exception as e:
             self.log_exception(e)
             return None
-
-    def get_network_citation(self, network_code, network_start_year):
-        try:
-            # Request citation info based on network_code
-            cite = self._request_citations(network_code)
-
-            # If citation info could not be found just on network_code, do a
-            # second request, this time including network_start_year
-            if cite is None:
-                cite = self._request_citations(network_code, network_start_year)
-
-            # If both requests failed, return None
-            if cite is None:
-                return None
-            return cite
-        except Exception as e:
-            self.log_exception(e)
-            return None
-
-    def _request_citations(self, network_code, network_start_year=None):
-        query = network_code
-
-        if network_start_year:
-            query += "_{}".format(network_start_year)
-
-        response = self._request(
-            "http://www.fdsn.org/networks/citation/?networks={}".format(query)
-        )
-        decoded = response.decode("utf-8")
-
-        soup = BeautifulSoup(decoded)
-        citations = soup.find_all("pre", attrs={"class": "citation"})
-        if len(citations) <= 0:
-            return None
-        if citations[0].text.lower() == "none":
-            return None
-        return citations[0].text
 
     def _request(self, url):
         try:
@@ -91,6 +50,8 @@ class DOIHelper(StationBookLoggerMixin):
 
             if response.info().get("Content-Encoding") == "gzip":
                 return gzip.decompress(response.read())
+            elif response.headers.get('content-type') == 'application/json':
+                return json.loads(response.read())
             else:
                 return response.read()
         except Exception:
