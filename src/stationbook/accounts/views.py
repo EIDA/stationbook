b4cda11b696b51784009327815080f96831976fc
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2023-04-04 14:53:48 +0200

    Replace Google Captcha with hCaptcha

--- src/stationbook/accounts/views.py
+++ src/stationbook/accounts/views.py
@@ -13,40 +13,32 @@ from django.views.generic import UpdateView
 
 from .forms import SignUpForm
 
+
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            ''' Begin reCAPTCHA validation '''
-            recaptcha_response = request.POST.get('g-recaptcha-response')
-            url = 'https://www.google.com/recaptcha/api/siteverify'
-            values = {
-                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-                'response': recaptcha_response
-            }
-            data = urllib.parse.urlencode(values).encode()
-            req =  urllib.request.Request(url, data=data)
-            response = urllib.request.urlopen(req)
-            result = json.loads(response.read().decode())
-            ''' End reCAPTCHA validation '''
-
-            if result['success']:
-                user = form.save()
-                auth_login(request, user)
-                return redirect('home')
-            else:
-                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
-                return render(request, 'signup.html', {'form': form})
+            user = form.save()
+            auth_login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Please make sure captcha is filled!")
+            return render(request, "signup.html", {"form": form})
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, "signup.html", {"form": form})
 
-@method_decorator(login_required, name='dispatch')
+
+@method_decorator(login_required, name="dispatch")
 class UserUpdateView(UpdateView):
     model = User
-    fields = ('first_name', 'last_name', 'email', )
-    template_name = 'my_account.html'
-    success_url = reverse_lazy('my_account')
+    fields = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+    template_name = "my_account.html"
+    success_url = reverse_lazy("my_account")
 
     def get_object(self):
         return self.request.user
